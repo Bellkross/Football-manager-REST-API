b9ac86c8947a636b58b191c720b2ced559db4a8f
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-05-01 17:12:23 +0300

    Rearrange code in CrudDao
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/gl/procamp/bellkross/football/dao/CrudDao.java
+++ src/main/java/com/gl/procamp/bellkross/football/dao/CrudDao.java
@@ -1,16 +1,18 @@
 package com.gl.procamp.bellkross.football.dao;
 
-import com.gl.procamp.bellkross.football.model.Team;
-
 import java.util.Collection;
 import java.util.Optional;
 
 public interface CrudDao<Entity, Id> {
 
     Collection<Entity> findAll();
+
     Optional<Entity> findById(Id id);
+
     Entity save(Entity entity);
+
     void deleteById(Id id);
+
     void delete(Entity entity);
 
 }
