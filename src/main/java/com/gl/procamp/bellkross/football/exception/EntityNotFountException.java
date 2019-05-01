d0dc790bddff55d966e702489c761e4d68382250
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-05-01 10:10:33 +0300

    Add entity not found exception
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/gl/procamp/bellkross/football/exception/EntityNotFountException.java
@@ -0,0 +1,7 @@
+package com.gl.procamp.bellkross.football.exception;
+
+public class EntityNotFountException extends RuntimeException {
+    public EntityNotFountException(String message) {
+        super(message);
+    }
+}
