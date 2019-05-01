1801028da1dab23f5223ba1a2a12aad27e71de0f
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-05-01 09:06:48 +0300

    Implement model classes
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/gl/procamp/bellkross/football/model/Player.java
+++ src/main/java/com/gl/procamp/bellkross/football/model/Player.java
@@ -1,4 +1,34 @@
 package com.gl.procamp.bellkross.football.model;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Player {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String position;
+    @Column(nullable = false)
+    private Date birthday;
+    @ManyToOne
+    private Team team;
+
 }
--- src/main/java/com/gl/procamp/bellkross/football/model/Team.java
+++ src/main/java/com/gl/procamp/bellkross/football/model/Team.java
@@ -1,8 +1,27 @@
 package com.gl.procamp.bellkross.football.model;
 
+import lombok.*;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Team {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @OneToOne(optional = false)
+    private Player captain;
 
 }
