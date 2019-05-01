0cc737d31b553b543b6e9c947d169c9ff41b9d2c
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-05-01 10:17:33 +0300

    Remove redundant Exception
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/gl/procamp/bellkross/football/exception/EntityNotFountException.java
+++
@@ -1,7 +0,0 @@
-package com.gl.procamp.bellkross.football.exception;
-
-public class EntityNotFountException extends RuntimeException {
-    public EntityNotFountException(String message) {
-        super(message);
-    }
-}
