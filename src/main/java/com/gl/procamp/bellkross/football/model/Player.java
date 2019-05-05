acf361df29f8bca9d339e9fd05238ae6894f145b
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-05-06 00:59:49 +0300

    Modify generation type to identity
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/gl/procamp/bellkross/football/model/Player.java
+++ src/main/java/com/gl/procamp/bellkross/football/model/Player.java
@@ -16,7 +16,7 @@ public class Player {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false)
     private String firstName;
--- src/main/java/com/gl/procamp/bellkross/football/model/Team.java
+++ src/main/java/com/gl/procamp/bellkross/football/model/Team.java
@@ -17,7 +17,7 @@ public class Team {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false)
     private String name;
