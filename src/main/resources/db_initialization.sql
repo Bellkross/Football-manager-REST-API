127585768063382bf7b200475a35ce862c12ca4e
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-05-05 23:32:21 +0300

    Add db initialization and schema script
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/resources/db_initialization.sql
@@ -0,0 +1,76 @@
+drop function if exists get_player_role_by_number(number bigint);
+create function get_player_role_by_number(number bigint)
+    returns text as
+$body$
+declare
+    roles text[] = '{"GOALKEEPER", "DEFENDER", "MIDFIELDER", "FORWARD"}';
+begin
+    number = number % 11;
+    if number = 0 then
+        return (select roles[1]);
+    elseif
+                number > 0 and number <= 2 then
+        return (select roles[2]);
+    elseif number > 2 and number <= 6 then
+        return (select roles[3]);
+    elseif number > 6 and number <= 11 then
+        return (select roles[4]);
+    end if;
+end;
+$body$
+    language 'plpgsql' VOLATILE
+    COST 100;
+
+truncate table team cascade;
+truncate table player cascade;
+do $$
+    declare
+        free_team_id bigint := 0;
+        team_name_template text := 'teamName%s';
+
+        free_player_id bigint := 0;
+        player_first_name_template text := 'firstName%s';
+        player_last_name_template text := 'lastName%s';
+        current_birthday timestamp := to_timestamp(0);
+
+        current_first_name text = '';
+        current_last_name text = '';
+        current_position text = '';
+        current_team_name text = '';
+        teams_count int := 10;
+        players_per_team int := 11;
+        current_captain_id int := 0;
+        counter int := 0;
+    begin
+        while free_team_id < teams_count loop
+            counter = 0;
+            while counter < players_per_team loop
+                current_first_name = (select format(player_first_name_template, free_player_id));
+                current_last_name = (select format(player_last_name_template, free_player_id));
+                current_position = (select get_player_role_by_number(free_player_id));
+
+                insert into player (id, firstname, lastname, position, birthday, team_id)
+                values (free_player_id, current_first_name, current_last_name, current_position, current_birthday, null);
+
+                counter = counter + 1;
+                free_player_id = free_player_id + 1;
+            end loop;
+
+            free_player_id = free_player_id - 11;
+            current_captain_id = free_player_id;
+            current_team_name = (select format(team_name_template, free_team_id));
+            insert into team (id, name, captain_id)
+            values (free_team_id, current_team_name, current_captain_id);
+
+            counter = 0;
+            while counter < players_per_team loop
+                update player set team_id = free_team_id where id = free_player_id;
+                counter = counter + 1;
+                free_player_id = free_player_id + 1;
+            end loop;
+
+            free_team_id = free_team_id + 1;
+        end loop;
+    end $$;
+
+drop function if exists get_player_role_by_number(number bigint);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+drop table if exists team cascade;
+drop table if exists player cascade;
+
+create table if not exists team
+(
+    id         integer      not null,
+    name       varchar(255) not null,
+    captain_id integer      not null,
+    constraint team_pk primary key (id)
+);
+
+create table if not exists player
+(
+    id         integer      not null,
+    first_name varchar(255) not null,
+    last_name  varchar(255) not null,
+    position   varchar(100) not null,
+    birthday   timestamp    not null,
+    team_id    integer      null,
+    constraint player_pk primary key (id),
+    constraint player_fk foreign key (team_id) references team (id)
+);
+
+alter table team
+    add constraint team_fk foreign key (captain_id) references player (id);
